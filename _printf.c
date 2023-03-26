6e25127ac2ffffa876b5d56aa956444edc799d4e
Andre victoria <122878923+Saint8083@users.noreply.github.com>
2023-03-26 17:58:43 +0100

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -7,6 +7,7 @@ void print_buffer(char buffer[], int *buff_ind);
  * @format: format.
  *
  * Return: Printed chars.
+ *
  */
 int _printf(const char *format, ...)
 {
@@ -57,6 +58,7 @@ int _printf(const char *format, ...)
  * print_buffer - Prints the contents of the buffer if it exist
  * @buffer: Array of chars
  * @buff_ind: Index at which to add next char, represents the length.
+ *
  */
 void print_buffer(char buffer[], int *buff_ind)
 {
